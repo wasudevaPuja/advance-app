271de41e57bb2a5eb418c547675470f5b657ca08
wasudewa puja <wasudewa223@gmail.com>
2025-12-09 22:46:11 +0800

    update letak img

--- app/app.vue
+++ app/app.vue
@@ -10,21 +10,3 @@
     <Footer />
   </div>
 </template>
-
-<script setup>
-const handleScroll = (e, target) => {
-  e.preventDefault()
-
-  const el = document.querySelector(target)
-  if (!el) return
-
-  const yOffset = -80 // tinggi navbar
-  const y = el.getBoundingClientRect().top + window.pageYOffset + yOffset
-
-  window.scrollTo({
-    top: y,
-    behavior: 'smooth'
-  })
-}
-</script>
-
